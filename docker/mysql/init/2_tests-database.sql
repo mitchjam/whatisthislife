128a6cad8aa498606d298bed070dc829b13e225b
Mitch <mitch@mitchs-mbp-2.mynetworksettings.com>
2023-02-20 12:36:18 -0500

    Init

---
+++ docker/mysql/init/2_tests-database.sql
@@ -0,0 +1,3 @@
+-- Database used to run the application's tests.
+-- Format taken from https://github.com/laravel/framework/blob/8.x/src/Illuminate/Database/Schema/Grammars/MySqlGrammar.php#L39
+CREATE DATABASE kf_test DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
